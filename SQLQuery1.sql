6aa571f1807117da252317b54ad2b53e9833381f
Reid Nolan <errornosignal@users.noreply.github.com>
2018-02-13 13:08:04 -0600

    end of class 02 13 2018

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -120,14 +120,14 @@
 --WHERE ProductName = 'Tofu';
 
 
-SELECT * --DISTINCT E.FirstName + ' ' +  E.LastName AS 'Employee Name', P.ProductName, P.ProductID, OD.OrderID, O.CustomerID, C.CompanyName, C.ContactName, E.EmployeeID
-FROM Products AS P FULL OUTER JOIN [Order Details] AS OD
-ON P.ProductID = OD.ProductID  FULL OUTER JOIN Orders AS O
-ON OD.OrderID = O.OrderID FUll OUTER JOIN Customers AS C
-ON O.CustomerID = C.CustomerID FUll OUTER JOIN Employees AS E
-ON O.EmployeeID = E.EmployeeID
-WHERE ProductName = 'Tofu' --AND O.OrderID = 0;
-ORDER BY ProductName
+--SELECT * --DISTINCT E.FirstName + ' ' +  E.LastName AS 'Employee Name', P.ProductName, P.ProductID, OD.OrderID, O.CustomerID, C.CompanyName, C.ContactName, E.EmployeeID
+--FROM Products AS P FULL OUTER JOIN [Order Details] AS OD
+--ON P.ProductID = OD.ProductID  FULL OUTER JOIN Orders AS O
+--ON OD.OrderID = O.OrderID FUll OUTER JOIN Customers AS C
+--ON O.CustomerID = C.CustomerID FUll OUTER JOIN Employees AS E
+--ON O.EmployeeID = E.EmployeeID
+--WHERE ProductName = 'Tofu' --AND O.OrderID = 0;
+--ORDER BY ProductName
 
 
 
@@ -142,6 +142,47 @@ ORDER BY ProductName
 --ORDER BY P.ProductName, [Employee Name]
 
 
+--SELECT* FROM Orders;
+
+--SELECT Count(OrderID) AS 'Total Orders'
+--FROM Orders
+--WHERE OrderDate > '1996-06-30' AND OrderDate < '1996-08-01';
+
+--SELECT OrderID, ProductID, COUNT(ProductID) AS 'Unique Items', SUM(Quantity) AS 'Total Items'
+--FROM [Order Details]
+--GROUP BY OrderID, ProductID with CUBE
+--HAVING SUM(Quantity) > 3
+
+--SELECT OrderID, COUNT(ProductID) AS 'Total'
+--FROM [Order Details]
+--GROUP BY OrderID
+--HAVING COUNT(ProductID) < 3;
 
 
 
+--SELECT Distinct E.FirstName + ' ' + E.LastName AS 'Employee Name', E.EmployeeID, SUM(OD.UnitPrice * OD.Quantity) AS 'Total'
+--FROM Employees AS E JOIN Orders AS O
+--ON E.EmployeeID = O.EmployeeID JOIN [Order Details] AS OD
+--ON O.OrderID = OD.OrderID JOIN Products AS P
+--ON OD.ProductId  = P.ProductID
+--Group By  E.EmployeeID, E.FirstName + ' ' + E.LastName
+--Having  P.ProductName = 'Tofu'
+--ORDER BY [Employee Name]
+
+
+--SELECT P.ProductName, P. ProductID, COUNT(O.OrderID) AS 'Total Orders'
+--FEOM Orders AS O JOIN 
+--ON E.EmployeeID = O.EmployeeID JOIN [Order Details] AS OD
+--ON O.OrderID = OD.OrderID JOIN Products AS P 
+--ON OD.ProductID = P.ProductID
+--WHERE P.ProductName = 'Tofu'
+--GROUP BY P.ProductName, P. ProductID
+
+
+--SELECT P.ProductID, P.ProductName, SUM(OD.UnitPrice * OD.Quantity) AS 'Total'
+--FROM Orders AS O JOIN [Order Details] AS OD
+--ON O.OrderID = OD.OrderID JOIN Products AS P
+--ON OD.ProductID = P.ProductID
+--GROUP BY P.ProductID, P.ProductName
+--Having SUM(OD.UnitPrice * OD.Quantity) < 5000
+--Order BY Total
