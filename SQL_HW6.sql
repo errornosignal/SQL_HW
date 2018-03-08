bcedeadddabceeb4dfe8af7e7911604b1a13e268
errornosignal <errornosignal@users.noreply.github.com>
2018-03-08 13:33:06 -0600

    03 08 2018 13:32 HW6 init commit

---
+++ SQL_HW6.sql
@@ -0,0 +1,45 @@
+﻿--Reid Nolan
+--Homework 6 – SQL
+
+use MyGuitarShop;
+
+--1. Write a SELECT statement that returns these columns from the Products table:
+--a. The ListPrice column
+--b. A column that uses the CAST function to return the ListPrice column
+--with 1 digit to the right of the decimal point
+--c. A column that uses the CONVERT function to return the ListPrice column
+--as an integer
+--d. A column that uses the CAST function to return the ListPrice column as
+--an integer
+
+
+
+--2. Write a SELECT statement that returns these columns from the Products table:
+--a. The DateAdded column
+--b. A column that uses the CAST function to return the DateAdded column
+--with its date only (year, month, and day)
+--c. A column that uses the CAST function to return the DateAdded column
+--with its full time only (hour, minutes, seconds, and milliseconds)
+--d. A column that uses the CAST function to return the DateAdded column
+--with just the month and day
+
+
+
+--3. Write a SELECT statement that returns these colums from the Orders table:
+--a. A column that uses the CONVERT function to return the OrderDate column
+--in this format: MM/DD/YYYY. In other words, use 2-digit months and
+--days and a 4-digit year, and separate each date component with
+--slashes.
+--b. A column that uses the CONVERT function to return the OrderDate column
+--with the date, and the hours and minutes on a 12-hour clock with an
+--am/pm indicator.
+--c. A column that uses the CONVERT function to return the OrderDate column
+--with 2-digit hours, minutes, and seconds on a 24-hour clock. Use
+--leading zeros for all date/time components.
+
+
+
+--Submitting your program -
+--You will submit this assignment on blackboard. You are allowed one submission.
+--Submit your .sql file and a document (*.docx or *.rtf) that contains screen shots
+--of the query results.
