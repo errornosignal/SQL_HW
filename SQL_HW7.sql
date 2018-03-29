d229bb07dc1727c8bed41bce3e095d1d28a22754
errornosignal <errornosignal@users.noreply.github.com>
2018-03-29 12:13:35 -0500

    03 29 2018 12:13 start hw7

---
+++ SQL_HW7.sql
@@ -0,0 +1,35 @@
+﻿--Reid Nolan
+--Homework 7 – SQL
+
+--1. Write a SELECT statement that returns these columns from the Products table:
+-- The ListPrice column
+-- The DiscountPercent column
+-- A column named DiscountAmount that uses the previous two columns to calculate the discount amount and uses the ROUND function to round the result to 2 decimal places
+
+
+--2. Write a SELECT statement that returns these columns from the Orders table:
+-- The OrderDate column
+-- A column that returns the four-digit year that’s stored in the OrderDate column
+-- A column that returns only the day of the month that’s stored in the OrderDate column.
+-- A column that returns the result from adding thirty days to the OrderDate column.
+
+
+--3. Write a SELECT statement that returns these columns from the Orders table:
+-- The CardNumber column
+-- The length of the CardNumber column
+-- The last four digits of the CardNumber column
+-- When you get that working right, add the column that follows to the result set. This is more difficult because the column requires the use of functions within functions.
+-- A column that displays the last four digits of the CardNumber column in this format: XXXX-XXXX-XXXX-1234. In other words, use Xs for the first 12 digits of the card number and actual numbers for the last four digits of the number.
+
+
+--4. Write a SELECT statement that returns these columns from the Orders table:
+-- The OrderID column
+-- The OrderDate column
+-- A column named ApproxShipDate that’s calculated by adding 2 days to the OrderDate column
+-- The ShipDate column
+-- A column named DaysToShip that shows the number of days between the order date and the ship date
+-- When you have this working, add a WHERE clause that retrieves just the orders for March 2016.
+
+
+--Submitting your program -
+--You will submit this assignment on blackboard. You are allowed one submission. Submit your .sql file and a document (*.docx or *.rtf) that contains screen shots of the query results.
